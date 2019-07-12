d6e14ed8cf0e8f107924fb8b75637a85fa4120e9
Michael Peterson <WebDev@ctoms.ca>
2019-07-12 11:25:00 -0600

    updated dependencies

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
 const http = require('http');
 const soap = require('soap');
+
+let helloService = {
+	HelloService:{
+		HelloPort:{
+			sayHello: function(args){
+				return{
+					message: "Hello " + args.name
+				}
+			}
+		}
+	}
+};
+
+let xml = require('fs').readFileSync('helloService.wsdl','utf-8');
+
+let server = http.createServer(function(req,res){
+	res.end("404: not found " + req.url);
+});
+
+server.listen(8000);
+
+let soapServer = soap.listen(server,'/helloservice',helloService,xml);
+soapServer.log = function(type, data) {
+    // type is 'received' or 'replied'
+    console.log('Type: ' + type + ' data: ' + data);
+};
+//console.log("test");
--- client.js
+++ client.js
@@ -1,1 +1,16 @@
 const soap = require('soap');
+const url = "http://localhost:8000/helloservice?wsdl";
+let args = {name:"test"};
+
+soap.createClient(url,function(err,client) {
+	//console.log(err);
+	if(err){
+		console.log("error occured ", err)
+	}else{
+		client.sayHello(args,function(err,result) {
+	    	console.log(result);
+	    	console.log(client.describe());
+  		});
+	}
+  
+});
